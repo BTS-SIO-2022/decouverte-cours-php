49c6f1ccd878c56040dc143f0a1341731a81f886
mathilderenversade <mathirenversade@gmail.com>
2023-01-10 10:08:39 +0100

    intial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+// J'affiche un commentaire sur une seule ligne en php
+
+/* 
+Je suis un commentaire php sur
+plusieurs 
+lignes
+*/
+
+// Pour afficher une chaîne de caractère
+
+echo 'Coucou je fais du php';
+
+// J'affiche du code html
+
+echo '<h2>Je suis un h2 </h2>';
+
+// Une variable c'est une boîte (un espace de mémoire) dans laquelle je viens stocker une information, une donnée. 
+$prenom = 'Mathilde'; 
+
+echo $prenom;
+
+$prenom = 'Juliette';
+
+echo $prenom;
+
+/* en php (comme en java), nous avons différents types de données (Data types)
+- string
+- integer
+- boolean
+- float
+- array
+- object
+- undefined / NULL
+*/
+
+?>
